997ff98c16f0244bc51478e679be95ff837318c2
dadan suwandi <Triciptainterga@gmail.com>
2024-06-24 18:13:01 +0800

    Report SF

---
+++ Apps/Apps/SF_2019.aspx.vb
@@ -0,0 +1,40 @@
+﻿Imports System
+Imports System.Data
+Imports System.Data.SqlClient
+Imports System.Drawing
+Imports System.IO
+Imports System.Security.Cryptography
+Imports DevExpress.XtraPrinting
+Public Class SF_2019
+    Inherits System.Web.UI.Page
+
+    Dim comm, com, sqlcom, sqlcomTo As SqlCommand
+    Dim sqlcon As New SqlConnection(ConfigurationManager.ConnectionStrings("DefaultConnection").ConnectionString)
+    Dim con As New SqlConnection(ConfigurationManager.ConnectionStrings("DefaultConnection").ConnectionString)
+    Dim sqlConnect As New SqlConnection(ConfigurationManager.ConnectionStrings("DefaultConnection").ConnectionString)
+    Dim sql As String = String.Empty
+    Dim sqldr, read, sqlDtr As SqlDataReader
+    Dim execute As New ClsConn
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        tempTrxSLA.SelectCommand = "SELECT * FROM SalesForce_2019 ORDER BY ID ASC"
+    End Sub
+    Private Sub ASPxGridView1_Init(sender As Object, e As EventArgs) Handles ASPxGridView1.Init
+        tempTrxSLA.SelectCommand = "SELECT * FROM SalesForce_2019 ORDER BY ID ASC"
+    End Sub
+
+    Private Sub ASPxGridView1_Load(sender As Object, e As EventArgs) Handles ASPxGridView1.Load
+        tempTrxSLA.SelectCommand = "SELECT * FROM SalesForce_2019 ORDER BY ID ASC"
+    End Sub
+    Private Sub btn_Export_Click(sender As Object, e As EventArgs) Handles btn_Export.Click
+        Dim casses As String = ddList.SelectedValue
+        Select Case casses
+            Case "xlsx"
+                Dim options As New XlsExportOptions
+                ASPxGridViewExporter1.WriteXlsxToResponse("SalesForce_2019_" & DateTime.Now.ToString("yyyyMMddhhmmss"))
+            Case "xls"
+                ASPxGridViewExporter1.WriteXlsToResponse("SalesForce_2019_" & DateTime.Now.ToString("yyyyMMddhhmmss"))
+            Case "csv"
+                ASPxGridViewExporter1.WriteCsvToResponse("SalesForce_2019_" & DateTime.Now.ToString("yyyyMMddhhmmss"))
+        End Select
+    End Sub
+End Class
